3a5646a347decad9a2b80a3a72c65bfc9a118901
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-12-07 10:18:23 -0300

    adicionando botões

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/nav.module.css'
+import { useRouter } from 'next/router'
 
 export default function Nav(){
+    const router = useRouter()
+
+    if(router.pathname == '/'){
+        return (
+            <ul className={styles.ul}>
+                <li className={styles.li}>
+                    <Link href='/' className={styles.link}><h5>Beer Clicker</h5></Link>
+                </li>
+                <div className={styles.botoes}>
+                    <a href='/usuario/login' type="button" className="btn btn-primary btn-sm">Sig in</a>
+                    <a href='/usuario/cadastro' type="button" className="btn btn-success btn-sm">Sig up</a>
+                </div>
+            </ul>
+        )
+    }
+
     return (
         <ul className={styles.ul}>
             <li className={styles.li}>
-                Beer Clicker
+                <Link href='/' className={styles.link}><h5>Beer Clicker</h5></Link>
             </li>
         </ul>
     )
