3f2f2b3365e5f8025302d65e0d3ea50f7e08c12f
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-12-11 10:51:19 -0300

    teste de nav bar

--- components/main.js
+++ components/main.js
@@ -1,11 +1,12 @@
 import Footer from './footer'
 import Nav from './nav'
 import styles from '../styles/main.module.css'
+import Navigation from './navigation'
 
 export default function Main({children}){
     return (
         <>  
-            <Nav/>
+            <Navigation/>
             <div className={styles.main}><div>{children}</div></div>
             <Footer/>
         </>
