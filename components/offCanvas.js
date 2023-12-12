e80633b7a7b304e8b1c969a3ac9fa41f16de6549
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-12-12 20:15:39 -0300

    alterações no offCanvas

--- components/offCanvas.js
+++ components/offCanvas.js
@@ -4,53 +4,62 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import { auth } from '../util/firebase';
 import { Image } from 'react-bootstrap';
 import styles from '../styles/offCanvas.module.css'
+import { uploadFoto } from '../api/gerenciaFoto';
 
 export default function OffCanvas(){
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
     const nomeUser = auth.currentUser.displayName === null ? 'Jogador' : auth.currentUser.displayName
-  
-    return (
-      <>
-        <Image src='/profile.png' width={40} height={40} onClick={handleShow}/>
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton className={styles.header}>
-            <Image src='/profile.png' alt='profile' width={100} height={100} />
-            <Offcanvas.Title>Bem-vindo, {nomeUser}</Offcanvas.Title>            
-          </Offcanvas.Header>
-          <hr/>
-          <Offcanvas.Body className={styles.body}>
-            <details>
-              <summary>Atualizar foto</summary>
-              <Foto/>
-            </details>
-            <details>
-              <summary>Ceveja favorita</summary>
-              <Foto/>
-            </details>
-            <details>
-              <summary>Seu ranking</summary>
-              <Foto/>
-            </details>
-          </Offcanvas.Body>
-        </Offcanvas>
-      </>
-    );
+    const foto = auth.currentUser.photoURL
+
+    if(foto){
+      return (
+        <>
+          <Image src={foto} width={40} height={40} style={{}} onClick={handleShow}/>
+          <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton className={styles.header}>
+              <Image src={foto} alt='profile' width={100} height={100} />
+              <Offcanvas.Title>Bem-vindo, {nomeUser}</Offcanvas.Title>            
+            </Offcanvas.Header>
+            <hr/>
+            <Offcanvas.Body className={styles.body}>
+              <details>
+                <summary>Atualizar foto</summary>
+                <Foto nick={nomeUser} handleShow={handleShow} handleClose={handleClose}/>
+              </details>
+              <details>
+                <summary>Ceveja favorita</summary>
+                <Foto/>
+              </details>
+              <details>
+                <summary>Seu ranking</summary>
+                <Foto/>
+              </details>
+            </Offcanvas.Body>
+          </Offcanvas>
+        </>
+      )
+    }else{
+      return(<></>)
+    }
 }
 
-export function Foto(){
-  const enviar = () =>{
-    console.log("enviou")
+export function Foto({nick, handleClose, handleShow}){
+  const [foto, setFoto] = useState()
+  const enviar = (e) =>{
+    e.preventDefault()
+    uploadFoto(nick, foto)
+    handleShow()
+    handleClose()
+    
   }
 
   return(
       <div className={styles.enviarFoto}>
           <form onSubmit={enviar} className={styles.form}>
               <label  className="form-label">Escolha uma imagem pro perfil</label>
-              <input className="form-control" type="file" />
+              <input className="form-control" type="file" onChange={(e) => setFoto(e.target.files[0])} name={nick} accept="image/*"/>
               <Button className={styles.Button} type='submit'>Enviar</Button>
           </form>
       </div>
