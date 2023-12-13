aff767cd52d30db821c9ae74b07f0476c1cb586c
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-12-13 10:26:10 -0300

    envio de foto feito

--- components/offCanvas.js
+++ components/offCanvas.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { auth } from '../util/firebase';
 import { Image } from 'react-bootstrap';
 import styles from '../styles/offCanvas.module.css'
 import { uploadFoto } from '../api/gerenciaFoto';
+
 
 export default function OffCanvas(){
     const [show, setShow] = useState(false);
@@ -12,13 +12,13 @@ export default function OffCanvas(){
     const handleShow = () => setShow(true);
     const nomeUser = auth.currentUser.displayName === null ? 'Jogador' : auth.currentUser.displayName
     const foto = auth.currentUser.photoURL === null ? '/profile.png' : auth.currentUser.photoURL
-
+  
     return (
       <>
-        <Image src={foto} className={styles.imgNav} onClick={handleShow}/>
+        <Image src={foto} alt='profile' className={styles.imgNav} onClick={handleShow}/>
         <Offcanvas show={show} onHide={handleClose}>
           <Offcanvas.Header closeButton className={styles.header}>
-            <Image src={foto} alt='profile' width={100} height={100} />
+            <Image className={styles.imgOffCanvas} src={foto} alt='profile' />
             <Offcanvas.Title>Bem-vindo, {nomeUser}</Offcanvas.Title>            
           </Offcanvas.Header>
           <hr/>
@@ -27,14 +27,13 @@ export default function OffCanvas(){
               <summary>Atualizar foto</summary>
               <Foto nick={nomeUser} handleShow={handleShow} handleClose={handleClose}/>
             </details>
+            <hr/>
             <details>
               <summary>Ceveja favorita</summary>
-              <Foto/>
+              
             </details>
-            <details>
-              <summary>Seu ranking</summary>
-              <Foto/>
-            </details>
+            <hr/>
+            <h6>Seu ranking 1º</h6>
           </Offcanvas.Body>
         </Offcanvas>
       </>
@@ -55,8 +54,8 @@ export function Foto({nick, handleClose, handleShow}){
       <div className={styles.enviarFoto}>
           <form onSubmit={enviar} className={styles.form}>
               <label  className="form-label">Escolha uma imagem pro perfil</label>
-              <input className="form-control" type="file" onChange={(e) => setFoto(e.target.files[0])} name={nick} accept="image/*"/>
-              <Button className={styles.Button} type='submit'>Enviar</Button>
+              <input className="form-control form-control-sm" type="file" onChange={(e) => setFoto(e.target.files[0])} name={nick} accept="image/*"/>
+              <button className="btn btn-outline-warning btn-sm" type='submit'>Enviar</button>
           </form>
       </div>
   )
