24236696ef4eb3ec20e6523926749cac6818a626
MarlonHD <marlonsilva840@gmail.com>
2023-12-14 02:54:43 -0300

    Atualização das APIs de acesso ao firebase

--- api/userMelhoriasApi.js
+++ api/userMelhoriasApi.js
@@ -1,5 +1,5 @@
-import { getFirestore, collection, getDocs, setDoc } from 'firebase/firestore';
-import { bd } from '../util/firebase';
+import { getFirestore, doc, collection, query, where, getDocs, setDoc, getDoc } from 'firebase/firestore';
+import { bd, app } from '../util/firebase';
 import { getMelhorias } from './melhoriasApi';
 
 //export async function getMelhorias() {
@@ -13,13 +13,38 @@ import { getMelhorias } from './melhoriasApi';
 //    return melhorias;
 //}
 
-export async function createUserMelhorias(id_nick){
+
+export async function getUserMelhorias(nick){
+    const firestore = getFirestore(app);
+    const statusCollection = collection(firestore, 'user_melhorias');
+    const statusQuery = query(statusCollection, where('nick', '==', nick));
+    const statusSnapshot = await getDocs(statusQuery);
+
+    const statusData = statusSnapshot.docs.map(doc => doc.data());
+    return statusData[0]; 
+}
+
+export async function getUserByEmail(email) {
+
+    const firestore = getFirestore(app);
+    const usersCollection = collection(firestore, 'user_melhorias');
+    const userQuery = query(usersCollection, where('email', '==', email));
+    const userSnapshot = await getDocs(userQuery);
+
+    const userData = userSnapshot.docs.map(doc => doc.data());
+    return userData[0]; 
+}
+
+export async function createUserMelhorias(id_nick, email){
     const melhorias = await getMelhorias()
 
     await setDoc(doc(bd, "user_melhorias", id_nick), {
-        email: {},
+        email: email,
+        foto: "",
         melhorias: melhorias.map(()=>0),
         click: 1,
+        total: 0,
+        nick: id_nick
     });
 }
 
