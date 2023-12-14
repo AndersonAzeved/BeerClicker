c3676a28cc8ce2c1448c8edcb1765f51e6b67406
MarlonHD <marlonsilva840@gmail.com>
2023-12-14 02:55:50 -0300

    Rota dinamica para cada usuario

---
+++ pages/jogo/[nick].js
@@ -0,0 +1,108 @@
+import { autenticar, auth, sair, bd } from "../../util/firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { useState, useEffect } from 'react';
+import { Button } from "react-bootstrap";
+import styles from "./styles/play.module.css"
+import { getMelhorias } from "../../api/melhoriasApi";
+import { useRouter } from "next/router";
+import { getAllUsers, getUserMelhorias } from "../../api/userMelhoriasApi";
+
+export default function Play(props){
+    const router = useRouter()
+    const { nick } = router.query
+
+    const [clock, setClock] = useState(0);
+    const [contador, setContador] = useState(0)
+
+   console.log(props)
+    useEffect(() => {
+        // Define um intervalo de 1000 milissegundos (1 segundo)
+        const intervalId = setInterval(() => {
+            // Atualiza o estado do contador a cada segundo
+            setClock((prevClock) => prevClock + 1);
+        }, 1000);
+
+        // Limpa o intervalo quando o componente é desmontado
+        return () => clearInterval(intervalId);
+    }, []); // O segundo parâmetro vazio [] garante que o useEffect seja executado apenas uma vez (montagem do componente)
+
+    const addContador = () =>{
+        setContador((a) => a+1)
+    }
+
+    const voltar = () =>{
+        sair()
+        router.push('/usuario/login')
+    }
+
+    if(autenticar()){
+        if(props == {}){
+            return(<div>carregando</div>)
+        }
+        return (
+            <div className={styles.layout}>
+                <div className={styles.container}>
+                    <p>Clock: {clock}</p>
+                    <Button onClick={voltar}>Sair</Button>
+                </div>
+                <div className={styles.container}>
+                    <p>Contador: {contador}</p>
+                    <img className={styles.beer} onClick={addContador} src="/beer.png"/>
+                </div>
+                <div className={styles.melhorias}>
+                    <div className={styles.container}>
+                        {props.melhorias.map((melhoria)=>(
+                            <div className={styles.item} key={melhoria.preco}>
+                                <img src={melhoria.foto} alt={melhoria.nome} />
+                                <div>{melhoria.nome}</div>
+                                <div>{melhoria.preco}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }else{
+        return(
+            <div>Error!!</div>
+        )
+    }
+}
+
+export async function getStaticPaths(){
+
+    return {
+        paths:[
+            {params: {nick: "joaozin"}},
+            {params: {nick: "Anderson Azevedo da Silva"}},
+            {params: {nick: "Marlin"}},
+        ],
+        fallback: true
+    }
+}
+
+export async function getStaticProps({ params }){
+    
+    try {
+        const estado = await getUserMelhorias(params.nick)
+        const melhorias = await getMelhorias()
+
+        if (!estado || !melhorias) {
+            return {
+                notFound: true,
+            };
+        }
+
+        return {
+            props: {
+                melhorias: melhorias,
+                estado: estado
+            },
+        };
+    } catch (error) {
+        console.error("Erro ao buscar dados do usuario:", error);
+        return {
+            notFound: true,
+        };
+    }
+}
